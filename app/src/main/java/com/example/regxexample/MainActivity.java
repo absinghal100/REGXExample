1c544913896346247f5be47bf283fcb815cdb86d
Abhishek Singhal <abhishek.singhal@mcnsolutions.net>
2022-08-03 22:24:45 +0530

    second update

--- app/src/main/java/com/example/regxexample/MainActivity.java
+++ app/src/main/java/com/example/regxexample/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.regxexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +16,10 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "Hello java", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "Hello java", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "Hello java", Toast.LENGTH_SHORT).show();
+
+        Log.v("Java","MyTag");
+        Log.d("Java","MyTag");
+        Log.e("Java","MyTag");
 
     }
 }
